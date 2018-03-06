10c72e16493c13811374450278b2e36c110b484b
Patric Steiner <patric.steiner1@gmail.com>
2018-03-06 23:12:02 +0100

    use local doubles instead of a volatile int array

--- src/Example2.java
+++ src/Example2.java
@@ -1,26 +1,26 @@
 public class Example2 {
 
-    private static volatile int[] array;
 
     public static void main(String[] args) {
         Stopwatch sw = new Stopwatch("Example 2: Instruction parallelism");
 
+        double a = 0, b = 0;
         int n = 100_000_000;
-        array = new int[2];
 
-        sw.start(440);
+        sw.start(197);
         for (int i = 0; i < n; i++) {
-            array[0]++;
-            array[0]++;
+            a += .1;
+            a += .1;
         }
         sw.stop();
 
-        sw.start(220);
+        sw.start(95);
         for (int i = 0; i < n; i++) {
-            array[0]++;
-            array[1]++;
+            a += .1;
+            b += .1;
         }
         sw.stop();
+        System.out.println("a = " + a + ", b = " + b);
     }
 
 }
